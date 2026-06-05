bcd37dc50c69baea0093707566d33ffa8d810530
이원재 <lwoj3019515@gmail.com>
2026-06-06 03:04:40 +0900

    add JAVA_HOME to run-local.sh for consistent local execution

--- run-local.sh
+++ run-local.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export JAVA_HOME=/opt/homebrew/opt/openjdk@17
+export PATH=$JAVA_HOME/bin:$PATH
+
 export DB_URL=jdbc:postgresql://localhost:5432/vexhibition
 export DB_USERNAME=root
 export DB_PASSWORD=1234

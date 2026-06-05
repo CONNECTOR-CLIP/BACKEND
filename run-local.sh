a8032212b4305b89ef23cfdc6f7cfdda715bb0dc
이원재 <lwoj3019515@gmail.com>
2026-06-05 21:25:11 +0900

    chore: 로컬 실행 스크립트 추가 (run-local.sh)

---
+++ run-local.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export DB_URL=jdbc:postgresql://localhost:5432/vexhibition
+export DB_USERNAME=root
+export DB_PASSWORD=1234
+export JWT_SECRET_KEY=RmFrZVNlY3JldEtleUZvckpXVEFwcGxpY2F0aW9uVGVzdGluZw==
+export JWT_EXPIRATION_TIME=3600000
+export PYTHON_API_URL=http://localhost:8000
+./gradlew bootRun
